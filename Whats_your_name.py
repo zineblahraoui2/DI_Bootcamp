2c92779148a6ce99a053a0b1c74492d586e7d2fc
zineblahraoui@uit.ac.ma <zineblahraoui@uit.ac.ma>
2025-10-31 17:47:52 +0100

    "Initial commit"

---
+++ Whats_your_name.py
@@ -0,0 +1,6 @@
+name_of_someone = input(" write your name ")
+My_name ="Zineb"
+if name_of_someone == My_name :
+    print(" That's a funny coincidence! ")
+else:
+    print(" OK! We don't have the same name. ")
