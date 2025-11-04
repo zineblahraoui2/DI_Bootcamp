e3cb6808e98ff0ce0581e95e354ff3eb157bdd5f
zineblahraoui@uit.ac.ma <zineblahraoui@uit.ac.ma>
2025-11-04 14:16:00 +0100

    solve_excercise1_Daily_Challenge

---
+++ week1/day2/Daily_Challenge/Lists_&_Strings.py
@@ -0,0 +1,8 @@
+number = int(input("Enter a number"))
+length =int(input("Enter a length"))
+list_of_multiple_numbers = []
+for i in range(1,length +1):
+    list_of_multiple_numbers.append(number*i)
+print(list_of_multiple_numbers)    
+        
+    
