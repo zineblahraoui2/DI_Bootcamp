0e25161e49ac29e10718abe2cbc7d7227e28ac1b
zineblahraoui@uit.ac.ma <zineblahraoui@uit.ac.ma>
2025-11-10 00:49:24 +0100

    Keyword_arguments

---
+++ week1/day4/Exercises_XP/Let’s_Create_Some_Personalized_Shirts.py
@@ -0,0 +1,7 @@
+def make_shirt(size="large",text="I love Python"):
+    print(f"The size of the shirt is {size} and the text is {text}.")
+
+make_shirt(text="I love my self")
+make_shirt(size="large")
+make_shirt(size="medium")
+make_shirt(size="small",text="Hello")
