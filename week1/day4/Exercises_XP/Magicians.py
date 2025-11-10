e82e684c84f290642e52b4ca5aa554537e9fbae8
zineblahraoui@uit.ac.ma <zineblahraoui@uit.ac.ma>
2025-11-10 12:37:31 +0100

    add_function_make_great

--- week1/day4/Exercises_XP/Magicians.py
+++ week1/day4/Exercises_XP/Magicians.py
@@ -1,4 +1,9 @@
 def show_magicians(magician_names): 
     for name in magician_names:
-        print(name)
-show_magicians(['Harry Houdini', 'David Blaine', 'Criss Angel'])      
+        name
+show_magicians(['Harry Houdini', 'David Blaine', 'Criss Angel']) 
+def make_great(magician_names):   
+    for  name in magician_names:
+        print(f"the Great {name}")
+make_great(['Harry Houdini', 'David Blaine', 'Criss Angel'])  
+     
