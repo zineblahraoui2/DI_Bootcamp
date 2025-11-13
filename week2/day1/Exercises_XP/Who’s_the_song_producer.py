8606defc1c7f4d5631aaf43cabc642f9535c90c6
zineblahraoui@uit.ac.ma <zineblahraoui@uit.ac.ma>
2025-11-13 17:10:02 +0100

    Add_Song_class_with_lyrics_and_ sing_me_a_song method

--- week2/day1/Exercises_XP/Who’s_the_song_producer.py
+++ week2/day1/Exercises_XP/Who’s_the_song_producer.py
@@ -1,11 +1,11 @@
 class Song:
     def __init__(self,lyrics):
         self.lyrics = list(lyrics)
-
+        
     def sing_me_a_song(self):
         for lyric in  self.lyrics :
             print(lyric)
-        
+
 stairway = Song(["There’s a lady who's sure", "all that glitters is gold", "and she’s buying a stairway to heaven"])
 stairway.sing_me_a_song()      
 
