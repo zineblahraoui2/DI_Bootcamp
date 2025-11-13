ef8fb348456673980e77057fa2304edbe9578353
zineblahraoui@uit.ac.ma <zineblahraoui@uit.ac.ma>
2025-11-14 00:11:15 +0100

    add_Farm_class_with_animal_manage_ment_and_info_methods

--- week2/day1/Daily_challenge/Old_MacDonald’s_Farm.py
+++ week2/day1/Daily_challenge/Old_MacDonald’s_Farm.py
@@ -13,11 +13,29 @@ class Farm:
     def get_info(self):
         print(f"{self.farm_name}\n")
         for animal_type,count in self.animals.items():
-            print(f"{animal_type}:{count}") 
+            print(f"{animal_type}:{count}")
+        print("   E-I-E-I-0!")
+    
+    def get_animal_types(self):
+        dict_keys = list(self.animals.keys())
+        dict_keys.sort()
+        return dict_keys
+        
+    def get_short_info(self):
+        list_of_animals = self.get_animal_types()
+        string_of_animals = " ".join(list_of_animals)
+        last_word = string_of_animals.split()[-1]
+        before_and_word = string_of_animals.split()[-2]
+        print(f"{self.farm_name} farm has {before_and_word} and {last_word}")
+        
 
-object_Farm = Farm("Happiness") 
-object_Farm.add_animal('cow', 2)
-object_Farm.add_animal('cow', 4)
+object_Farm = Farm("peas") 
+object_Farm.add_animal('cow',2)
+object_Farm.add_animal('cow',4)
+object_Farm.add_animal('bird',5)
+object_Farm.add_animal('dog',5)
 object_Farm.get_info()
+print(object_Farm.get_animal_types())
+object_Farm.get_short_info()
 
       
