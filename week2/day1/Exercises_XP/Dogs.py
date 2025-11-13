81e04a48dbd771474cd20e01ec816a4164c10a18
zineblahraoui@uit.ac.ma <zineblahraoui@uit.ac.ma>
2025-11-13 17:07:40 +0100

    Refactor_Dog_class_and_add_ function_to_compare_dogs'_heights

--- week2/day1/Exercises_XP/Dogs.py
+++ week2/day1/Exercises_XP/Dogs.py
@@ -2,7 +2,6 @@ class Dog:
     def __init__(self, name, height):
         self.name = name
         self.height = height
-
     def bark(self):
         print(f"{self.name} goes woof!")  
 
@@ -10,12 +9,10 @@ class Dog:
         x = self.height * 2
         print(f"{self.name} jumps {x} cm high!") 
 
-
 def comparing_height_of_dogs(dog1, dog2):
     height_dogs = [dog1, dog2]
     comparison = max(height_dogs, key=lambda dog: dog.height)
     return comparison
-
 
 davids_dog = Dog("REX", 10)    
 sarahs_dog = Dog("FOX", 40)
@@ -30,7 +27,6 @@ print(davids_dog.name)
 print(davids_dog.height)
 print(sarahs_dog.name)
 print(sarahs_dog.height)
-
 davids_dog.bark()
 sarahs_dog.jump()
 
